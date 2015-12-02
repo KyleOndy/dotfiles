58993e2c667e40b079978fb6b05dd9b548ef9de3
Kyle Ondy <KyleOndy@gmail.com>
2015-12-02 11:21:51 -0500

    add paths

--- zsh/zsh/path.zsh
+++ zsh/zsh/path.zsh
@@ -2,6 +2,8 @@ typeset -U path
 
 path=(~/.cabal/bin "$path[@]")
 path=(~/.rbenv/bin "$path[@]")
+path=(~/.gem/ruby/2.2.0/bin "$path[@]")
+path=(~/.local/bin "$path[@]")
 
 # prune paths that don't exist
 path=($^path(N))
