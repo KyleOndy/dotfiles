7534a7da7404a24a88d39136351885d218702847
Kyle Ondy <Kyle@Ondy.me>
2016-03-24 11:08:09 -0400

    Split my fish config to increase maintainability
    Put everything into separate files to keep like responsibilities
    together.

---
+++ apps/fish/.config/fish/functions/export.fish
@@ -0,0 +1,26 @@
+# Language settings
+set -x LC_ALL en_US.UTF-8
+set -x LC_CTYPE en_US.UTF-8
+
+# respect local bins
+set -x PATH "./bin" $PATH
+
+# editor
+set -x EDITOR "nvim"
+
+# LESS with colors
+# from http://blog.0x1fff.com/2009/11/linux-tip-color-enabled-pager-less.html
+set -x LESS "-RSM~gIsw"
+
+# Colorful man pages
+# from http://pastie.org/pastes/206041/text
+setenv -x LESS_TERMCAP_mb (set_color -o red)
+setenv -x LESS_TERMCAP_md (set_color -o red)
+setenv -x LESS_TERMCAP_me (set_color normal)
+setenv -x LESS_TERMCAP_se (set_color normal)
+setenv -x LESS_TERMCAP_so (set_color -b blue -o yellow)
+setenv -x LESS_TERMCAP_ue (set_color normal)
+setenv -x LESS_TERMCAP_us (set_color -o green)
+
+# grep colors
+setenv -x GREP_OPTIONS "--color=auto"
