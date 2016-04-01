e690a992d0a1fd630ae5bf3bda9d79111faddf97
Kyle Ondy <Kyle@Ondy.me>
2016-04-01 13:18:37 -0400

    Graceful handling of fish.config.local
    Will create a blank placeholder if the local config does not exist to
    the shell script will not error out,

--- setup.fish
+++ setup.fish
@@ -1,7 +1,13 @@
 #!/usr/bin/env fish
-
 set BASE_DIR (cd (dirname (status -f)); and pwd)
 
 eval $BASE_DIR/check.sh | grep missing
 eval $BASE_DIR/dots.sh > /dev/null
+
+
+set LOCAL_FIST_CONFIG "$HOME/.config/fish/config.fish.local"
+if test ! -e $LOCAL_FIST_CONFIG
+  touch $LOCAL_FIST_CONFIG
+end
+
 . "$HOME/.config/fish/config.fish"
