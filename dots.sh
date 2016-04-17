fba552f78ed9efd9fff0fd76c493ff2068b8ee92
Kyle Ondy <Kyle@Ondy.me>
2016-04-16 22:07:57 -0400

    Use the archive flag instead of recursive
    Its better practice.

--- dots.sh
+++ dots.sh
@@ -6,5 +6,5 @@ BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for d in `find "$BASE_DIR/apps/" -maxdepth 1 -mindepth 1 -type d`
 do
 	echo $d
-	cp -frv --symbolic-link $d/. ~/
+	cp -fav --symbolic-link $d/. ~/
 done
