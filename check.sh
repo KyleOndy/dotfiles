7c99bf28bdd45b6b447bf723b5b26ad1f7ce2278
Kyle Ondy <kyle@ondy.me>
2016-04-17 22:58:50 -0400

    Stop script on unset varibales

--- check.sh
+++ check.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-set -e
+set -eu
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 check_program(){
--- dots.sh
+++ dots.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -eu
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
