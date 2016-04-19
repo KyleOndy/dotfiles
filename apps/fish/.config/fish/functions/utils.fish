bd59ad1f4d95acc30bc9c897c7c93a84b39a651c
Kyle Ondy <kyle@ondy.me>
2016-04-19 08:21:34 -0400

    Add branch to fish prompt

--- apps/fish/.config/fish/functions/utils.fish
+++ apps/fish/.config/fish/functions/utils.fish
@@ -1,1 +1,41 @@
 alias myip="curl -s checkip.dyndns.org | grep -Eo '[0-9\.]+'"
+
+function update_dots
+  cd ~/.dotfiles; and git stash save and git pull; and ./dots.sh
+end
+
+function fish_prompt --description 'Write out the prompt'
+    # Just calculate these once, to save a few cycles when displaying the prompt
+  if not set -q __fish_prompt_hostname
+      set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+  end
+
+  if not set -q __fish_prompt_normal
+    set -g __fish_prompt_normal (set_color normal)
+  end
+
+  if not set -q __git_cb
+    set __git_cb ":"(set_color brown)(git branch ^/dev/null | grep \* | sed 's/* //')(set_color normal)""
+  end
+
+  switch $USER
+
+  case root
+    if not set -q __fish_prompt_cwd
+      if set -q fish_color_cwd_root
+        set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
+    else
+      set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+    end
+  end
+
+  printf '%s@%s:%s%s%s%s# ' $USER $__fish_prompt_hostname "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" $__git_cb
+
+  case '*'
+    if not set -q __fish_prompt_cwd
+      set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+  end
+
+  printf '%s@%s:%s%s%s%s$ ' $USER $__fish_prompt_hostname "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" $__git_cb
+  end
+end
