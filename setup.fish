2aee1dc71d1996ccd16cc48356265fd55c069b7b
Kyle Ondy <kyle@ondy.me>
2016-07-14 11:56:51 -0400

    misspelled variable

--- setup.fish
+++ setup.fish
@@ -5,8 +5,8 @@ eval $BASE_DIR/check.sh | grep missing
 eval $BASE_DIR/dots.sh > /dev/null
 
 
-set LOCAL_FIST_CONFIG "$HOME/.config/fish/config.fish.local"
-if test ! -e $LOCAL_FIST_CONFIG
+set LOCAL_FISH_CONFIG "$HOME/.config/fish/config.fish.local"
+if test ! -e $LOCAL_FISH_CONFIG
   touch $LOCAL_FIST_CONFIG
 end
 
