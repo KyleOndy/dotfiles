05ed57700d5c7aaa0ab3d21d55af2e3f7d46391b
Kyle Ondy <kyle@ondy.me>
2016-07-26 10:06:25 -0400

    fix gnupg permissions

--- setup.fish
+++ setup.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 set BASE_DIR (cd (dirname (status -f)); and pwd)
 
-eval $BASE_DIR/check.sh | grep missing
+#eval $BASE_DIR/check.sh | grep missing
 eval $BASE_DIR/dots.sh > /dev/null
 
 
@@ -9,5 +9,9 @@ set LOCAL_FISH_CONFIG "$HOME/.config/fish/config.fish.local"
 if test ! -e $LOCAL_FISH_CONFIG
   touch $LOCAL_FISH_CONFIG
 end
+
+chown (whoami):(whoami) ~/.gnupg/gpg.conf
+chmod 600 ~/.gnupg/*
+chmod 700 ~/.gnupg
 
 . "$HOME/.config/fish/config.fish"
