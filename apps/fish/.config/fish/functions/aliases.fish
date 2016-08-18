b7de8e69c930317ad7f0560101241dde1f302998
Kyle Ondy <kyle@ondy.me>
2016-08-18 10:53:09 -0400

    treea alias wasn't showing hidden files

--- apps/fish/.config/fish/functions/aliases.fish
+++ apps/fish/.config/fish/functions/aliases.fish
@@ -10,12 +10,12 @@ function tree5; tree -L 5 $argv; end
 function tree6; tree -L 6 $argv; end
 
 alias treea "tree -a -I '.git|.stack-work' --dirsfirst -ChFQ"
-function treea1; tree -L 1 $argv; end
-function treea2; tree -L 2 $argv; end
-function treea3; tree -L 3 $argv; end
-function treea4; tree -L 4 $argv; end
-function treea5; tree -L 5 $argv; end
-function treea6; tree -L 6 $argv; end
+function treea1; treea -L 1 $argv; end
+function treea2; treea -L 2 $argv; end
+function treea3; treea -L 3 $argv; end
+function treea4; treea -L 4 $argv; end
+function treea5; treea -L 5 $argv; end
+function treea6; treea -L 6 $argv; end
 
 
 function bigdirs; du -h --max-depth=1 $argv | sort -h; end
