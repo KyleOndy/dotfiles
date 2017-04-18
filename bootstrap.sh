0893b24c821c173ca63999325028006f4446756f
Kyle Ondy <kondy@cclind.com>
2017-04-18 12:11:00 -0400

    fix curl command

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ apt-get install -y \
     curl \
     gnupg2
 
-curl /tmp/install.sh https://raw.githubusercontent.com/KyleOndy/dotfiles/install_script/install.sh > /tmp/install.sh
+curl https://raw.githubusercontent.com/KyleOndy/dotfiles/install_script/install.sh > /tmp/install.sh
 #curl -o /tmp/install.sh.asc https://raw.githubusercontent.com/KyleOndy/dotfiles/install_script/install.sh.asc
 #gpg2 --verify install.sh
 
