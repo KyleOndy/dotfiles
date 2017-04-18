36279fcdf9392e59f9628f181d755af9aab1399c
Kyle Ondy <kondy@cclind.com>
2017-04-18 12:00:41 -0400

    wip

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,5 +8,8 @@ apt-get install -y \
     gnupg2
 
 curl -o /tmp/install.sh https://raw.githubusercontent.com/KyleOndy/dotfiles/install_script/install.sh
-curl -o /tmp/install.sh.asc https://raw.githubusercontent.com/KyleOndy/dotfiles/install_script/install.sh.asc
-gpg2 --verify install.sh
+#curl -o /tmp/install.sh.asc https://raw.githubusercontent.com/KyleOndy/dotfiles/install_script/install.sh.asc
+#gpg2 --verify install.sh
+
+chmod +x ./install.sh
+./install.sh sources
