4035ba4c296ad10d279e24de698bd64754845224
Kyle Ondy <kondy@cclind.com>
2017-04-19 13:33:13 -0400

    added echos when scripts are installed

--- install.sh
+++ install.sh
@@ -312,16 +312,19 @@ install_scripts() {
   # install speedtest
   curl -sSL https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py  > /usr/local/bin/speedtest
   chmod +x /usr/local/bin/speedtest
+  echo "Installed speedtest"
 
   # install icdiff
   curl -sSL https://raw.githubusercontent.com/jeffkaufman/icdiff/master/icdiff > /usr/local/bin/icdiff
   curl -sSL https://raw.githubusercontent.com/jeffkaufman/icdiff/master/git-icdiff > /usr/local/bin/git-icdiff
   chmod +x /usr/local/bin/icdiff
   chmod +x /usr/local/bin/git-icdiff
+  echo "Installed icdiff"
 
   # install lolcat
   curl -sSL https://raw.githubusercontent.com/tehmaze/lolcat/master/lolcat > /usr/local/bin/lolcat
   chmod +x /usr/local/bin/lolcat
+  echo "installed lolcat"
 }
 
 # install syncthing
