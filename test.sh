e4a631cbce95be556d6f6a2a49ef114ede2a7b9a
Kyle Ondy <kondy@cclind.com>
2017-04-20 06:48:42 -0400

    add simple testing

---
+++ test.sh
@@ -0,0 +1,21 @@
+#1/usr/bin/env bash
+
+set -euo pipefail
+
+ERRORS=()
+
+for f in $(find . -type f -not -iwholename '*.git*' | sort -u); do
+  if file "$f" | grep --quiet shell; then {
+    shellcheck "$f" && echo "[OK]: sucesfully linted $f"
+    } || {
+    ERRORS+=("$f")
+    }
+  fi
+done
+
+if [ ${#ERRORS[@]} -eq 0 ]; then
+  echo "No errors, yay!"
+else
+  echo "These files failed shellcheck: ${ERRORS[*]}"
+  exit 1
+fi
