78c8533a807be837342757dc4ec590f8349cf4a5
Kyle Ondy <kyle@ondy.org>
2019-11-14 14:01:06 -0500

    Add i3 configuration to home-manager.
    The window manager and xsession configuration should be on a user level
    and not a system level.

--- home/i3.nix
+++ home/i3.nix
@@ -6,9 +6,16 @@
     windowManager.i3 = {
       enable = true;
       config = {
+        modifier = "Mod4"; # super
         workspaceAutoBackAndForth = true;
       };
     };
+  };
+
+  services.screen-locker = {
+    enable = true;
+    inactiveInterval = 5;
+    lockCmd = "/home/tuxinaut/.config/i3/i3-exit lock";
   };
 }
 
