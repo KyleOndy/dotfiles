a33a06f37c7135a2c79e8d8a774d516e3c3fedb5
Kyle Ondy <kyle@ondy.org>
2019-11-14 21:47:22 -0500

    Add note about initialHashedPassword.

--- hosts/_includes/kyle.nix
+++ hosts/_includes/kyle.nix
@@ -6,6 +6,8 @@
   users.users.kyle = {
     isNormalUser = true;
     extraGroups = [ "audio" "docker" "wheel" ]; # Enable ‘sudo’ for the user.
+    # despite being able to change the password, every time NixOS is rebuild,
+    # the password gets reset.
     initialHashedPassword =
       "$6$hYiIwvTIv$2Z3lBfOQYi4IymaU2CLW2UwJcLfAvtEt1zAw5LJ/qtWQ/rnDEVLmwtaTJW4iUfRAH9QjzV10rHm06wgqvSXWt1";
     shell = pkgs.zsh;
