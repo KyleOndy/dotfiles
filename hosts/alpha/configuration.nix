802f677f2917f319a641faaaf1e0b9c307cbcaf8
Kyle Ondy <kyle@ondy.org>
2019-11-24 10:32:34 -0500

    Enable webui's on laptop.

--- hosts/alpha/configuration.nix
+++ hosts/alpha/configuration.nix
@@ -20,19 +20,25 @@
     preLVM = true;
   }];
 
-  networking.hostName = "alpha";
-  #
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp0s31f6.useDHCP = true;
-  networking.interfaces.wlp4s0.useDHCP = true;
+  networking= {
+    hostName = "alpha";
+    #
+    # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+    # Per-interface useDHCP will be mandatory in the future, so this generated config
+    # replicates the default behaviour.
+    useDHCP = false;
+    interfaces.enp0s31f6.useDHCP = true;
+    interfaces.wlp4s0.useDHCP = true;
+    firewall = {
+      allowedTCPPorts = [ 80 443 ];
+    };
+  };
 
   hardware = { cpu.intel.updateMicrocode = true; };
 
   # no adhoc user managment
   users.mutableUsers = false;
+
 
   system.stateVersion = "19.09"; # Did you read the comment?
 
