49f9905e7fe0c7087412dcf67406b6bcaa489682
Kyle Ondy <kyle@ondy.org>
2019-11-27 20:35:27 -0500

    Update fonts

--- home/fonts.nix
+++ home/fonts.nix
@@ -10,14 +10,15 @@
     pkgs.fira-code
     pkgs.fira-code-symbols
     pkgs.fira-mono
-    pkgs.hack-font
+    pkgs.hack-font           # used in st
+    pkgs.nanum-gothic-coding # fallback in spacemacs
     pkgs.hasklig
     pkgs.hermit
     pkgs.inconsolata
     pkgs.mononoki
     pkgs.raleway
     pkgs.roboto
-    pkgs.source-code-pro
+    pkgs.source-code-pro    # used in spacemacs
     pkgs.terminus_font
   ];
 }
