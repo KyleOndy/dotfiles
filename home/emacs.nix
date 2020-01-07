c91f290b2af717ac61206dfd0961e81a8267d0c4
Kyle Ondy <kyle@ondy.org>
2020-01-06 23:41:17 -0500

    Add basic emacs plugins.

--- home/emacs.nix
+++ home/emacs.nix
@@ -4,6 +4,17 @@
   programs.emacs = {
     enable = true;
     package = pkgs.emacs;
+    extraPackages = epkgs:
+      with epkgs; [
+        # themes
+        epkgs.solarized-theme
+
+        # evil
+        epkgs.evil
+
+        # git
+        epkgs.magit
+      ];
   };
 }
 
