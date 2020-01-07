bb7f8c7946e18161895da31b50c8030d06bddd63
Kyle Ondy <kyle@ondy.org>
2020-01-06 23:40:47 -0500

    Fix issues with pinentry,

--- hosts/_includes/common.nix
+++ hosts/_includes/common.nix
@@ -36,12 +36,6 @@
   services.udev.packages = [ pkgs.yubikey-personalization ];
   services.pcscd.enable = true;
 
-  environment.shellInit = ''
-    export GPG_TTY="$(tty)"
-    gpg-connect-agent /bye
-    export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
-  '';
-
   programs = {
     ssh.startAgent = false;
     gnupg.agent = {
