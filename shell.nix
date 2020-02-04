86005949490e03be2bf01627c68a677ec811476a
Kyle Ondy <kyle@ondy.org>
2020-02-04 11:06:12 -0500

    Add tooling to better control this repository.
    Adding pre-commits will help enforce style of files and prevent my
    from pushing secrets to the world.

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    # for nixpkgs-fmt
+    nixpkgs-fmt
+    cargo
+
+    # pre-commit
+    # https://pre-commit.com/
+    pre-commit
+    detect-secrets
+  ];
+}
