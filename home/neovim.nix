03d0ff37be63a3a908df6508003ae39623cd4b2b
Kyle Ondy <kyle@ondy.org>
2020-02-13 08:29:24 -0500

    Cut undesired vim-terraform behavior
    I don't know why these two settings were changed from defaul. They were
    introduced in 069f62faf83549162fd7cdb2224d26553628ae9e and I am
    regretting my lack of commit messages. I assume I just copied and pasted
    some configuration I found somewhere online.
    
    I don't really like using folds, so why would I want to _just_ fold
    Terraform files and nothing else?
    
    Terraform recommends the octothorpe (#) character [1] as the comment string.
    Why was I trying to be special and used the non default?
    
    This commit bring me to a saner working space with Terraform.
    
    [1] https://www.terraform.io/docs/configuration/syntax.html#comments

--- home/neovim.nix
+++ home/neovim.nix
@@ -357,8 +357,6 @@ in
       " ## terraform
       " -------------------------------------------------------------
       let g:terraform_align=1
-      let g:terraform_fold_sections=1
-      let g:terraform_commentstring='//%s'
       let g:terraform_fmt_on_save=1
 
       " ## elm-vim setup
