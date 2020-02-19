3844b0b994405b1e4dce0e67343958457aa06009
Kyle Ondy <kyle@ondy.org>
2020-02-19 07:48:46 -0500

    perf: remove fonts that are not being used
    This change removes all the font that I tired at one time but no longer
    use. There is not reason to keep them installed and updated.
    
    I also set the base font to `Hack` to remove the need to keep
    `Lat2-Terminus16` around.

--- home/fonts.nix
+++ home/fonts.nix
@@ -3,22 +3,7 @@
 {
   fonts.fontconfig.enable = true;
 
-  # todo: I do not use all these. Remove what I don't use.
   home.packages = [
-    pkgs.fantasque-sans-mono
-    pkgs.fira
-    pkgs.fira-code
-    pkgs.fira-code-symbols
-    pkgs.fira-mono
     pkgs.hack-font # used in st
-    pkgs.nanum-gothic-coding # fallback in spacemacs
-    pkgs.hasklig
-    pkgs.hermit
-    pkgs.inconsolata
-    pkgs.mononoki
-    pkgs.raleway
-    pkgs.roboto
-    pkgs.source-code-pro # used in spacemacs
-    pkgs.terminus_font
   ];
 }
