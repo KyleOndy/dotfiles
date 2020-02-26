4b0192f089c2ad13241e291fc90e1c57016612ad
Kyle Ondy <kyle@ondy.org>
2020-02-26 08:54:34 -0500

    refactor: reduce verbosity by using `with pkgs; [`
    No need to have every package declaration have a `pkgs.` in front of it.

--- home/clojure.nix
+++ home/clojure.nix
@@ -1,13 +1,13 @@
 { pkgs, ... }:
 
 {
-  home.packages = [
-    pkgs.clj-kondo # linter
-    pkgs.clojure
-    pkgs.joker # linter
-    pkgs.leiningen # build tooling
+  home.packages = with pkgs; [
+    clj-kondo # linter
+    clojure
+    joker # linter
+    leiningen # build tooling
 
     # try using IntelliJ  and cursive
-    pkgs.jetbrains.idea-community
+    jetbrains.idea-community
   ];
 }
