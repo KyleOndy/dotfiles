439bc39feb18c5414f9c1af293dde4c147a2fdd5
Kyle Ondy <kyle@ondy.org>
2020-05-09 16:22:43 -0400

    feat: add ZSH binding and func to pick git commit
    The commit allows me to _very_ easily pick a git commit from a nicely
    formatted list and drop it right into my current command.
    
    The impetus for this command was my quest to short the time and lessen
    the effort needed to apply a set of changes in git to a previous commit.

--- home/zsh.nix
+++ home/zsh.nix
@@ -51,6 +51,27 @@
 
       # shell hooks
       eval "$(direnv hook zsh)"
+
+      # my quality of life functions
+      fzf_pick_git_commit() {
+        # I've already put a lot of time into my `git lg` alias an am very
+        # familiar with how it looks. I am just reusing most of the logic here.
+        # I force `--color` becuase git will output this without color by
+        # default.
+        LOG_LINE=$(git log --color --pretty=format:'%Cred%h%Creset -%G?-%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' | fzf --ansi)
+        # as the git commit ref is the first item in the line, split at the
+        # first space and take the first argument.
+        echo -n "$LOG_LINE" | cut -d' ' -f1 | tr -d $'\n'
+      }
+
+      # now <ctrl> + y gives me a nice popup to select a git commit hash from a
+      # nicely formatted list
+      _zle_pick_git_commit() {
+        COMMIT=$(fzf_pick_git_commit)
+        LBUFFER=$LBUFFER$COMMIT
+      }
+      zle -N _zle_pick_git_commit
+      bindkey '^y' _zle_pick_git_commit
     '';
   };
 }
