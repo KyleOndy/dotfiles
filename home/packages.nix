851a7e248a1c765ab70847f7afef0ac150e12355
Kyle Ondy <kyle@ondy.org>
2020-06-22 16:10:44 -0400

    fix: remove duplicate 'steam' entry from packages

--- home/packages.nix
+++ home/packages.nix
@@ -45,7 +45,6 @@
     proselint # A linter for prose
     pv # pipe progress
     ranger # cli file browser
-    steam # games
     remmina # remote desktop client
     ripgrep # recursively searches directories for a regex pattern
     shellcheck # linting bash scripts
@@ -55,7 +54,7 @@
     squashfsTools # create and unpack squashfs
     st # lightweight terminal
     stack # Haskell build tooling
-    steam # games
+    steam # games # todo: I should break this out into gaming.nix
     terraform # infrastructure as code
     terraform-docs # auto documentation generation
     tflint # better terraform linter
