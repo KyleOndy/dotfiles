2b70e5299dfa972667415de7d80b879fa7ba5f9e
Kyle Ondy <kyle@ondy.org>
2020-07-08 10:27:02 -0400

    feat: update go path to be outside of src path
    I don't do go much, and I don't really understand the implications of
    moving this path, but having the `GOPATH` be my source directory
    clutters everything with go's naming convention.

--- home/go.nix
+++ home/go.nix
@@ -1,13 +1,9 @@
 { pkgs, ... }:
 
-let
-  old_dots = import ./_dotfiles-dir.nix;
-
-in
 {
   programs.go = {
     enable = true;
     package = pkgs.go;
-    goPath = "src";
+    goPath = "go";
   };
 }
