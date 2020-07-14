7be13c7e9a99efa0176d0aa5afa5112e3bbf3ff5
Kyle Ondy <kyle@ondy.org>
2020-07-13 20:35:39 -0400

    feat(zsh): add more bindings for git + fzf
    This commits adds some binds with a "leader" go `g` to select get
    commits, tags, branches, and remotes.

--- home/zsh.nix
+++ home/zsh.nix
@@ -58,25 +58,6 @@
         fi
       }
 
-      fzf_pick_git_commit() {
-        # I've already put a lot of time into my `git lg` alias an am very
-        # familiar with how it looks. I am just reusing most of the logic here.
-        # I force `--color` becuase git will output this without color by
-        # default.
-        LOG_LINE=$(git log --color --pretty=format:'%Cred%h%Creset -%G?-%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' | fzf --ansi)
-        # as the git commit ref is the first item in the line, split at the
-        # first space and take the first argument.
-        echo -n "$LOG_LINE" | cut -d' ' -f1 | tr -d $'\n'
-      }
-
-      # now <ctrl> + y gives me a nice popup to select a git commit hash from a
-      # nicely formatted list
-      _zle_pick_git_commit() {
-        COMMIT=$(fzf_pick_git_commit)
-        LBUFFER=$LBUFFER$COMMIT
-      }
-      zle -N _zle_pick_git_commit
-      bindkey '^y' _zle_pick_git_commit
 
       # todo: not sure this is the best way to instll the theme
       mkdir -p "$HOME/.zfunctions"
@@ -86,6 +67,88 @@
       prompt spaceship
       export SPACESHIP_TIME_SHOW=true # show timestamps in prompt
       eval spaceship_vi_mode_enable # https://github.com/denysdovhan/spaceship-prompt/issues/799
+
+      # easily cycle through history with up and down arrow
+      bindkey "^[[A" history-beginning-search-backward
+      bindkey "^[[B" history-beginning-search-forward
+
+      # fancy git + fzf
+      # todo: refactor this into its own script and just source it
+      is_in_git_repo() {
+        git rev-parse HEAD > /dev/null 2>&1
+      }
+
+      _fzf() {
+        fzf "$@" --border
+      }
+
+      fzf_pick_git_commit() {
+        is_in_git_repo || return
+        # I've already put a lot of time into my `git lg` alias an am very
+        # familiar with how it looks. I am just reusing most of the logic here.
+        # I force `--color` becuase git will output this without color by
+        # default.
+        git log --color --pretty=format:'%Cred%h%Creset -%G?-%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' |
+        _fzf --ansi --no-sort --reverse --multi \
+          --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES |
+        grep -o "[a-f0-9]\{7,\}"
+      }
+
+      fzf_pick_git_tag() {
+        is_in_git_repo || return
+        git tag --sort -version:refname |
+        _fzf --multi --preview-window right:70% \
+          --preview 'git show --color=always {} | head -'$LINES
+      }
+
+      fzf_pick_git_remote() {
+        is_in_git_repo || return
+        git remote -v | awk '{print $1 "\t" $2}' | uniq |
+        _fzf --tac \
+          --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1} | head -200' |
+        cut -d$'\t' -f1
+      }
+
+      fzf_pick_git_branch() {
+        is_in_git_repo || return
+        git branch -a --color=always | grep -v '/HEAD\s' | sort |
+        _fzf --ansi --multi --tac --preview-window right:70% \
+          --preview 'git log --oneline --graph --date=short --color=always --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -'$LINES |
+        sed 's/^..//' | cut -d' ' -f1 |
+        sed 's#^remotes/##'
+      }
+
+      # A helper function to join multi-line output from fzf
+      join-lines() {
+        local item
+        while read item; do
+          echo -n "''${(q)item} "
+        done
+      }
+
+      fzf-git-commit-widget() {
+          LBUFFER+=$(fzf_pick_git_commit | join-lines)
+      }
+      zle -N fzf-git-commit-widget
+      bindkey '^g^g' fzf-git-commit-widget
+
+      fzf-git-tag-widget() {
+          LBUFFER+=$(fzf_pick_git_tag | join-lines)
+      }
+      zle -N fzf-git-tag-widget
+      bindkey '^g^t' fzf-git-tag-widget
+
+      fzf-git-remote-widget() {
+          LBUFFER+=$(fzf_pick_git_remote | join-lines)
+      }
+      zle -N fzf-git-remote-widget
+      bindkey '^g^r' fzf-git-remote-widget
+
+      fzf-git-branch-widget() {
+          LBUFFER+=$(fzf_pick_git_branch | join-lines)
+      }
+      zle -N fzf-git-branch-widget
+      bindkey '^g^b' fzf-git-branch-widget
     '';
   };
   home.packages = with pkgs;
