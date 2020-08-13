ccc20a291fbbf9c53cf9da7c6a21a0158a829463
Kyle Ondy <kyle@ondy.org>
2020-08-13 15:55:48 -0400

    feat(kbd): pinky escape is now left alt when held
    I make extensive use of the alt key within tmux and vim.

--- keyboard/keymap.c
+++ keyboard/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | MO(1)  |   A  |   S  |   D  |   F* |   G  |------|           |------|   H  |   J* |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------|  ,   |           |  .   |------+------+------+------+------+--------|
- * | Escape |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |      |      |      |   /    |
+ * |LAlt(Ex)|   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |      |      |      |   /    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | CTRL |  Alt |      | LGui |LSft(()|                                       |RSft())|Ctrl|      |  Alt | Ctrl   |
  *   `-----------------------------------'                                       `-----------------------------------'
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,     KC_2,   KC_3,     KC_4,   KC_5,   KC_MINS,      KC_EQL,   KC_6,   KC_7,   KC_8,     KC_9,     KC_0,     KC_NO,
   KC_TAB,   KC_Q,     KC_W,   KC_E,     KC_R,   KC_T,   KC_LBRC,      KC_RBRC,  KC_Y,   KC_U,   KC_I,     KC_O,     KC_P,     KC_BSLS,
   MO(1),    KC_A,     KC_S,   KC_D,     KC_F,   KC_G,                           KC_H,   KC_J,   KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-  KC_ESC,   KC_Z,     KC_X,   KC_C,     KC_V,   KC_B,   KC_COMM,      KC_DOT,   KC_N,   KC_M,   KC_COMM,  KC_DOT,   KC_NO,    KC_SLSH,
+  LALT_T(KC_ESC), KC_Z, KC_X, KC_C,     KC_V,   KC_B,   KC_COMM,      KC_DOT,   KC_N,   KC_M,   KC_COMM,  KC_DOT,   KC_NO,    KC_SLSH,
   KC_LCTL,  KC_LALT,  KC_NO,  KC_LGUI,  KC_LSPO,                                       KC_RSPC, KC_RCTL,  KC_NO,    KC_RALT,  KC_RCTL,
 
                                                 KC_NO,    KC_NO,      KC_NO,   KC_NO,
