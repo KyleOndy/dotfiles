a896cb85d7ecc9f657cbb456377bf4d39466e71e
Kyle Ondy <kyle@ondy.org>
2020-09-10 21:16:39 -0400

    feat: enable remote builder

--- hosts/_includes/common.nix
+++ hosts/_includes/common.nix
@@ -73,4 +73,16 @@
     cleanTmpDir = true;
     kernelPackages = pkgs.linuxPackages_latest;
   };
+
+  nix = {
+    distributedBuilds = true;
+    buildMachines = [
+      {
+        hostName = "eu.nixbuild.net";
+        system = "x86_64-linux";
+        maxJobs = 100;
+        supportedFeatures = [ "benchmark" "big-parallel" ];
+      }
+    ];
+  };
 }
