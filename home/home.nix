3809381670eb747c15f06c93e635750c280c31f7
Kyle Ondy <kyle@ondy.org>
2020-12-28 22:42:06 -0500

    overlays

--- home/home.nix
+++ home/home.nix
@@ -22,23 +22,6 @@
     ./zsh.nix
   ];
 
-  # import all the overlays that extend packages via nix or home-manager.
-  # Overlays are a nix file within the `overlay` folder or a sub folder in
-  # `overlay` that contains a `default.nix`.
-  nixpkgs.overlays =
-    let
-      path = ./overlays;
-    in
-    with builtins;
-    map (n: import (path + ("/" + n))) (
-      filter
-        (
-          n:
-          match ".*\\.nix" n != null
-          || pathExists (path + ("/" + n + "/default.nix"))
-        )
-        (attrNames (readDir path))
-    );
 
   programs = {
     home-manager = {
