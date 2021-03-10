3037018d48b5117d72af6a7020df3cc586b347f8
Kyle Ondy <kyle@ondy.org>
2021-03-10 10:25:18 -0500

    refactor: remove unused arguments

--- hosts/_includes/kvm.nix
+++ hosts/_includes/kvm.nix
@@ -1,6 +1,6 @@
 # This is a catchall for configuration items that are common across all
 # machines and at this time do not make sense to break out into their own file.
-{ config, pkgs, ... }:
+{ ... }:
 
 {
   boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
