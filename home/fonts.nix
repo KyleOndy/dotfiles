5ff9aa616f984781a6dba3bf8727d43f009838fb
Kyle Ondy <kyle@ondy.org>
2021-04-23 22:06:39 -0400

    feat(font): use nerdfont patched Iosevka and Hack
    NerdFont [1] patches fonts with a bunch of icons and glyps. These
    patched fonts are used in many new neovim plugins.
    
    I am still not sure if I like hack or iosevka better, so keeping both
    for now.
    
    [1]: https://www.nerdfonts.com/

--- home/fonts.nix
+++ home/fonts.nix
@@ -1,15 +1,13 @@
 { pkgs, ... }:
 
 {
-  # todo: why do I need to set mkForce?
-  # This change was needed when I cutover from using submodules to niv to track
-  # dependencies. Something in that process has seemed to change the order
-  # these settings are applied. When I understand NixOS/Nix better I'd like to
-  # circle back and look into this.
-  fonts.fontconfig.enable = pkgs.lib.mkForce true;
-
   home.packages = with pkgs; [
-    hack-font # used in st
-    iosevka
+    #  hack-font
+    (nerdfonts.override {
+      fonts = [
+        "Hack"
+        "Iosevka"
+      ];
+    })
   ];
 }
