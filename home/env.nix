841406ea2c3dc35be0bc45ec1d27a3630501402a
Kyle Ondy <kyle@ondy.org>
2021-05-22 11:12:29 -0400

    docs: update and improve various docs and comments

--- home/env.nix
+++ home/env.nix
@@ -2,10 +2,10 @@
 { pkgs, ... }:
 let
   # todo: I would like to do something like `${pkgs.neovim}/bin/nvim";`, but
-  # that is the system neovim binary without my configuration. If I run
-  # `readlink $(which nvim)` I get a path
-  # `/nix/store/...-home-manager-path/bin/nvim`. I need to do a bit of digging
-  # to figure out how to source that path.
+  #       that is the system neovim binary without my configuration. If I run
+  #       `readlink $(which nvim)` I get a path
+  #       `/nix/store/...-home-manager-path/bin/nvim`. I need to do a bit of
+  #       digging to figure out how to source that path.
   editor = "nvim";
   dotfiles = "$HOME/src/dotfiles";
 in
@@ -29,15 +29,12 @@ in
 
   home.sessionVariables = {
     DOTFILES = dotfiles;
-    # todo:
     EDITOR = editor;
     VISUAL = editor;
-    # $HOME/wip_scripts is where I put scripts that are not ready for inclsion
-    # in the source contro repo. I try hard to not let scripts sit there for
-    # too long.
-    PATH = "$PATH:${pkgs.my-scripts}/bin:$HOME/wip_scripts";
-    # this allows the rest of the system to default to the packages I use in my
-    # dotfiles.
+    # todo: shouldn't my-scripts be on path already?
+    PATH = "$PATH:${pkgs.my-scripts}/bin";
+    # this allows the rest of the nix tooling to use the same nixpkgs that I
+    # have set in the flake.
     NIX_PATH = "nixpkgs=${pkgs.path}";
     MANPAGER = "${pkgs.bash}/bin/bash -c 'col -bx | ${pkgs.bat}/bin/bat -l man -p'";
   };
