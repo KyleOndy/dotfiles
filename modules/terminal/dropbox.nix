b89c2a6b53ba76c9ec783adfda30e5b18dc0c3f3
Kyle Ondy <kyle@ondy.org>
2021-05-23 15:44:53 -0400

    feat: add dropbox module

---
+++ modules/terminal/dropbox.nix
@@ -0,0 +1,14 @@
+{ lib, config, ... }:
+with lib;
+let cfg = config.foundry.terminal.dropbox;
+in
+{
+  options.foundry.terminal.dropbox = {
+    enable = mkEnableOption "dropbox";
+  };
+  config = mkIf cfg.enable {
+    services = {
+      dropbox.enable = true;
+    };
+  };
+}
