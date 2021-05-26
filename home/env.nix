a5ca129702f75a6debf85aadf3a440ac6e4b7dec
Kyle Ondy <kyle@ondy.org>
2021-05-25 21:06:06 -0400

    chore: updating home/env.nix

--- home/env.nix
+++ home/env.nix
@@ -2,12 +2,13 @@
 { pkgs, ... }:
 let
   # todo: I would like to do something like `${pkgs.neovim}/bin/nvim";`, but
-  #       that is the system neovim binary without my configuration. If I run
-  #       `readlink $(which nvim)` I get a path
-  #       `/nix/store/...-home-manager-path/bin/nvim`. I need to do a bit of
-  #       digging to figure out how to source that path.
+  # that is the system neovim binary without my configuration. If I run
+  # `readlink $(which nvim)` I get a path
+  # `/nix/store/...-home-manager-path/bin/nvim`. I need to do a bit of digging
+  # to figure out how to source that path.
   editor = "nvim";
-  dotfiles = "$HOME/src/dotfiles";
+  homeDir = "$HOME"; # todo: does this work?
+  dotfiles = "${homeDir}/src/dotfiles";
 in
 {
   programs = {
@@ -17,5 +18,26 @@ in
         theme = "gruvbox-dark";
       };
     };
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+  };
+
+  home.sessionVariables = {
+    DOTFILES = dotfiles;
+    FOUNDRY_DATA = "${homeDir}/src/foundry";
+    # todo:
+    EDITOR = editor;
+    VISUAL = editor;
+    # $HOME/wip_scripts is where I put scripts that are not ready for inclsion
+    # in the source contro repo. I try hard to not let scripts sit there for
+    # too long.
+    # todo: shoudn't `my-scripts` be on path already?
+    PATH = "$PATH:${pkgs.my-scripts}/bin:$HOME/wip_scripts";
+    # this allows the rest of the system to default to the packages I use in my
+    # dotfiles.
+    NIX_PATH = "nixpkgs=${pkgs.path}";
+    MANPAGER = "${pkgs.bash}/bin/bash -c 'col -bx | ${pkgs.bat}/bin/bat -l man -p'";
   };
 }
