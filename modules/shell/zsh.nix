cbd5721c478b41d4b87e702a797c2a66250f75d4
Kyle Ondy <kyle.ondy@paige.ai>
2021-05-26 10:09:40 -0400

    feat: add support for building darwin system
    These are the changes needed to build my configuration on my work
    macbook.

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -25,6 +25,8 @@ in
         shellAliases = {
           ":e" = "$EDITOR";
           ":q" = "exit";
+          f = "foundry";
+          cdf = "cd $FOUNDRY_DATA";
           cdtmp = "cd $(mktemp -d)";
           lsd = "ls -l $@ | grep '^d'";
           llr = "ll --color=auto -t | head";
@@ -38,22 +40,16 @@ in
           include () {
             [[ -f "$1" ]] && source "$1"
           }
-
           # shell hooks
           eval "$(direnv hook zsh)"
-
           # zsh tweaks not included in home-manager.
           # todo: add these to home-manager and contribute upsteam
-
           # reduce <ESC> key timeout in vim mode
           export KEYTIMEOUT=50
-
           # change the auto-completion color
           # todo: there is probably a better way
           export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=6'
-
           # my quality of life functions
-
           # todo: not sure this is the best way to instll the theme
           mkdir -p "$HOME/.zfunctions"
           fpath=( "$HOME/.zfunctions" $fpath )
@@ -62,42 +58,32 @@ in
           prompt spaceship
           export SPACESHIP_TIME_SHOW=true # show timestamps in prompt
           eval spaceship_vi_mode_enable # https://github.com/denysdovhan/spaceship-prompt/issues/799
-
           # nicer autocomplete selections
           zstyle ':completion:*' menu select # use arrows to navigate autocomplete results
           zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}' # lowers match uppers
-
           # easily cycle through history with up and down arrow
           bindkey "^[[A" history-beginning-search-backward
           bindkey "^[[B" history-beginning-search-forward
-
           # I really want the vi binding to more vim like
           bindkey -v '^?' backward-delete-char # allow backspace key to work as expected
-
           # open the commnad line in $EDITOR
           autoload -z edit-command-line
           zle -N edit-command-line
           bindkey -M vicmd v edit-command-line
-
           # match my binding for [neo]vim
           bindkey -M viins 'jk' vi-cmd-mode
-
           setopt ignoreeof # don't close my shell on ^d. Why is that a good idea?
-
           # this is bound to '\ec' (alt-c) by defaul, but I really like having this
           # shortcut available on a control key combo
           bindkey '^e' fzf-cd-widget
-
           # fancy git + fzf
           # todo: refactor this into its own script and just source it
           is_in_git_repo() {
             git rev-parse HEAD > /dev/null 2>&1
           }
-
           _fzf() {
             fzf "$@" --multi --ansi --border
           }
-
           fzf_pick_git_commit() {
             is_in_git_repo || return
             # I've already put a lot of time into my `git lg` alias an am very
@@ -109,14 +95,12 @@ in
               --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES |
             grep -o "[a-f0-9]\{7,\}"
           }
-
           fzf_pick_git_tag() {
             is_in_git_repo || return
             git tag --sort -version:refname |
             _fzf --preview-window right:70% \
               --preview 'git show --color=always {} | head -'$LINES
           }
-
           fzf_pick_git_remote() {
             is_in_git_repo || return
             git remote -v | awk '{print $1 "\t" $2}' | uniq |
@@ -124,7 +108,6 @@ in
               --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1} | head -200' |
             cut -d$'\t' -f1
           }
-
           fzf_pick_git_branch() {
             is_in_git_repo || return
             git branch -a --color=always | grep -v '/HEAD\s' | sort |
@@ -133,7 +116,6 @@ in
             sed 's/^..//' | cut -d' ' -f1 |
             sed 's#^remotes/##'
           }
-
           # A helper function to join multi-line output from fzf
           join-lines() {
             local item
@@ -141,38 +123,32 @@ in
               echo -n "''${(q)item} "
             done
           }
-
           fzf-git-commit-widget() {
               LBUFFER+=$(fzf_pick_git_commit | join-lines)
           }
           zle -N fzf-git-commit-widget
           bindkey '^g^g' fzf-git-commit-widget
-
           fzf-git-tag-widget() {
               LBUFFER+=$(fzf_pick_git_tag | join-lines)
           }
           zle -N fzf-git-tag-widget
           bindkey '^g^t' fzf-git-tag-widget
-
           fzf-git-remote-widget() {
               LBUFFER+=$(fzf_pick_git_remote | join-lines)
           }
           zle -N fzf-git-remote-widget
           bindkey '^g^r' fzf-git-remote-widget
-
           fzf-git-branch-widget() {
               LBUFFER+=$(fzf_pick_git_branch | join-lines)
           }
           zle -N fzf-git-branch-widget
           bindkey '^g^b' fzf-git-branch-widget
-
           fzf_pick_aws_profile() {
-            aws_profile=$(grep '[.*]' "$HOME/.aws/credentials" | cut -c 2- | rev | cut -c 2- | rev | _fzf)
+            aws_profile=$(grep '\[profile .*\]' "$HOME/.aws/config" | cut -d' ' -f2 | rev | cut -c 2- | rev | _fzf)
             export AWS_PROFILE="$aws_profile"
           }
           zle -N fzf_pick_aws_profile
           bindkey '^a^p' fzf_pick_aws_profile
-
           # easily export which kube config I want. I can't break things if I can
           # not connect to the cluster.
           fzf_pick_kube_config() {
