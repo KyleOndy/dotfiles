ef06cad01310d5dacd33eb15694d36841398daee
Kyle Ondy <kyle@ondy.org>
2021-06-11 21:26:54 -0400

    feat(python): add python to module

--- modules/dev/python.nix
+++ modules/dev/python.nix
@@ -1,6 +1,9 @@
 { lib, pkgs, config, ... }:
 with lib;
-let cfg = config.foundry.dev.python;
+let
+  cfg = config.foundry.dev.python;
+  python-packages = python-packages: with python-packages; [ virtualenv ];
+  system-python-with-packages = pkgs.python3.withPackages python-packages;
 in
 {
   options.foundry.dev.python = {
@@ -8,6 +11,9 @@ in
   };
 
   config = mkIf cfg.enable {
-    home.packages = [ pkgs.nodePackages.pyright ];
+    home.packages = with pkgs; [
+      system-python-with-packages
+      nodePackages.pyright
+    ];
   };
 }
