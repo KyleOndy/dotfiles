8050b96e0676e30fde4531c916a520512c3f985c
Kyle Ondy <kyle@ondy.org>
2021-10-26 14:56:16 -0400

    feat(alpha): use gpg module

--- hosts/alpha/home.nix
+++ hosts/alpha/home.nix
@@ -6,7 +6,7 @@
     ./../../home/fonts.nix
     ./../../home/git.nix
     ./../../home/go.nix
-    ./../../home/gpg.nix
+    #./../../home/gpg.nix
     ./../../home/haskell.nix
     ./../../home/i3.nix
     ./../../home/packages.nix
@@ -68,6 +68,7 @@
         neovim.enable = true;
       };
       pass.enable = true;
+      gpg.enable = true;
     };
   };
 
