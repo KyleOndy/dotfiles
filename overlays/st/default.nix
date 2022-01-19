f8b24b839dea5f112d3b3deafa4169f107ab1a11
Kyle Ondy <kyle@ondy.org>
2022-01-18 21:12:22 -0500

    fix(st): fix patches to work with updated version

--- overlays/st/default.nix
+++ overlays/st/default.nix
@@ -4,11 +4,7 @@ self: super: {
       # set font to `hack`
       ./st-font_hack-20191111-75f92eb.diff
       # set colorscheme to `gruvbox`
-      ./st-gruvbox-20191111-75f92eb.diff
-    ] ++ builtins.map super.fetchurl [{
-      url = "https://st.suckless.org/patches/boxdraw/st-boxdraw_v2-0.8.2.diff";
-      sha256 =
-        "c1b7ab7672815b73e8328ecc55300c12fddce9ecae4ab04ff4377bd9132089f6";
-    }];
+      ./st-gruvbox-20220118-ded1e4c.diff
+    ];
   };
 }
