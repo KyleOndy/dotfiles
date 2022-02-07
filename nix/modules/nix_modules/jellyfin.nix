a5c018a012e14634759fb9ed6ce200bb0f8fc35e
Kyle Ondy <kyle@ondy.org>
2022-02-06 21:37:21 -0500

    feat(jellyfin): add backup to service

--- nix/modules/nix_modules/jellyfin.nix
+++ nix/modules/nix_modules/jellyfin.nix
@@ -1,6 +1,8 @@
 { lib, pkgs, config, ... }:
 with lib;
-let cfg = config.systemFoundry.jellyfin;
+let
+  cfg = config.systemFoundry.jellyfin;
+  stateDir = "/var/lib/jellyfin"; # todo: hardcoded
 in
 {
   options.systemFoundry.jellyfin = {
@@ -26,6 +28,22 @@ in
       default = "jellyfin";
       description = "User to server jellyfin under";
     };
+    backup = mkOption {
+      default = { };
+      description = "Move the backups somewhere";
+      type = types.submodule {
+        options.enable = mkOption {
+          type = types.bool;
+          default = false;
+          description = "Enable backup moving";
+        };
+        options.destinationPath = mkOption {
+          type = types.path;
+          default = "/var/backups/jellyfin";
+          description = "Specifies the directory backups will be moved too.";
+        };
+      };
+    };
   };
 
   config = mkIf cfg.enable {
@@ -42,6 +60,21 @@ in
     systemFoundry.nginxReverseProxy."${cfg.domainName}" = {
       enable = true;
       proxyPass = "http://127.0.0.1:8096";
+    };
+
+    # jellyfin provides no native backup, so zip, compress it, and copy it over
+    systemd.services.jellyfin-backup = mkIf cfg.backup.enable {
+      startAt = "*-*-* 3:00:00";
+      path = with pkgs;[
+        coreutils
+        gnutar
+        pigz
+      ];
+      script = ''
+        mkdir -p ${cfg.backup.destinationPath}
+        tar --use-compress-program="pigz -k --best" -cvf ${cfg.backup.destinationPath}/jellyfin-$(date +%Y-%m-%d).tar.gz ${stateDir}
+
+      '';
     };
   };
 }
