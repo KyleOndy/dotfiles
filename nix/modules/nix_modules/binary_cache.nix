b7e46a8bfd46449c5ea19eaf0692377c4e0618f4
Kyle Ondy <kyle@ondy.org>
2022-02-08 21:56:20 -0500

    feat: add binary cache module

---
+++ nix/modules/nix_modules/binary_cache.nix
@@ -0,0 +1,30 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.systemFoundry.binary_cache;
+in
+{
+  options.systemFoundry.binary_cache = {
+    enable = mkEnableOption ''
+      serve a binary cache
+    '';
+    domainName = mkOption {
+      type = types.str;
+      description = "Domain to server gitea under";
+    };
+  };
+
+  config = mkIf cfg.enable {
+
+    services = {
+      nix-serve = {
+        enable = true;
+      };
+    };
+    systemFoundry.nginxReverseProxy."${cfg.domainName}" = {
+      enable = true;
+      proxyPass = "http://127.0.0.1:${toString config.services.nix-serve.port}";
+    };
+  };
+}
+
+
