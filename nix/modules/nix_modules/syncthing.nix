f5c35374c7ae1472a593cd97e46c206206e18256
Kyle Ondy <kyle@ondy.org>
2022-09-08 13:45:11 -0400

    feat(syncthing): add dino

--- nix/modules/nix_modules/syncthing.nix
+++ nix/modules/nix_modules/syncthing.nix
@@ -21,6 +21,7 @@ in
       devices = {
         # to get syncthing key you need to install syncthing first.
         "alpha" = { id = "G5I3OAK-5O57DWJ-ZKPP6R3-J3KPCVB-EWMVN3L-UCV7ONL-TNOW2DO-3SJ3YAD"; };
+        "dino" = { id = "CUCD6QA-3GFH3P2-RKN5U7Z-OY5B42E-22BIG7I-C5NY75E-KG5JFAZ-K25MEQQ"; };
         "omega" = { id = "CWYMQLD-JVLVARV-WFEWGR7-URPPQF2-3AAMP5E-IJASRHK-S2OTCEP-PCWNGAF"; };
         "tiger" = { id = "MWIARYQ-NO6ZDWT-4KINIBB-RFVW57A-6EAIHJF-KAOL4RH-QR7XSIV-SRFMXQ5"; };
         "sharptooth" = { id = "Y7N4CUM-QTVMTPL-KB4M4WC-7ZCVHGQ-REOJ7A4-ZHFPRMK-HLTUUQW-HPCZQQR"; };
@@ -54,6 +55,7 @@ in
           path = "home/kyle/synced_scratch";
           devices = [
             "alpha"
+            "dino"
             "omega"
             "sharptooth"
             "tiger"
