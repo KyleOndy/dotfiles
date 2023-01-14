7d22050bf84cb17a6d0496ddbec55c28950adee1
Kyle Ondy <kyle@ondy.org>
2023-01-13 22:39:51 -0500

    feat(dino): add ssh key to all hosts

--- nix/users/kyle.nix
+++ nix/users/kyle.nix
@@ -14,6 +14,8 @@
     openssh.authorizedKeys.keys = [
       # juice ssh for android
       "ecdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBCY48p/4M/8CcfQgq/4J/bYRflVQ2MFovineycMxsEorlW50oOm1SJ8nn2qAAE75bxgbqbmFPBNdV1JUx/9DAnTITqw13lKdb09M2c59NQN6LjaL1SUbboiSxiwv6hHtAg== JuiceSSH"
+      # key from dino
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDKPwXdnorhTtQOZ0iE3YJHtb8YYfhjnaav8ArQQuIOQR4tAxPyxMucKHuTsCH3soFFBTY1wg0KVt4x+6op4bfhr0Q40bqQprwy/5LFmui1FZhFhAxrbx4abK0Kh6NaKjvYmV1Lh9+gSKTK9edxWixX90ZI6YHhVEf5JSeUbVcKYKMD4gp5CR5EC2l8/bd/4nQ3n74Od4faa4DfE4qaleEQ4IcAONR0WGxtX1aP2Q4V+UfbS2gvBA0c/V0eIIXnscMcqBbzrYPMxQ7a8umpA65ByHgdFBnCeyvhKjxl2E1HoZcPzruBXs/NqmvnhG6iuFDPtG2G+Lj6xjEYffJcI2VnkYAyczD63P6zlsBIPbyvq7aS8jGR0CsNbfJExjXLmB3M4k2ANBidfai26zAN/Pn73MOA9ieShy1FUZCYf3nM5+EO+0Al6v48eJXNrcUNqKRUHEdyRi+Sd3Nj5shZ61lgCdSZk78XUjXpWcmhbFGaR+9aXn3kUV5rDjqpLzp4alU= kyle@dino"
     ];
   };
 }
