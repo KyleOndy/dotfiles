0d180e5e6cda62a3d64e29dd02c0bf7889e0eb45
Kyle Ondy <kyle@ondy.org>
2023-02-14 22:33:02 -0500

    feat(tiger): add hardware decoding configuration
    From https://nixos.wiki/wiki/Jellyfin.

--- nix/hosts/tiger/configuration.nix
+++ nix/hosts/tiger/configuration.nix
@@ -257,6 +257,19 @@ in
       };
     };
 
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
+
   system.stateVersion = "21.11"; # Did you read the comment?
 }
 
