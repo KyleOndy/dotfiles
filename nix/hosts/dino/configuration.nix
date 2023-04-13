ff76a0e2c3ad45a93a571f9d8ad586fb4dd635a2
Kyle Ondy <kyle@ondy.org>
2023-04-12 20:33:30 -0400

    remove hidpi

--- nix/hosts/dino/configuration.nix
+++ nix/hosts/dino/configuration.nix
@@ -12,7 +12,6 @@
   # becuase we are dual booting
   time.hardwareClockInLocalTime = true;
   services.fwupd.enable = true;
-  hardware.video.hidpi.enable = false; # todo: this breaks fonts
 
   networking.hostName = "dino"; # Define your hostname.
   networking.wireless.enable = true;
