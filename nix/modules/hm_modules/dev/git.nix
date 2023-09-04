f53dd877c828bc1cc58b66bbd1f4b91535ae0705
Kyle Ondy <kyle@ondy.org>
2023-09-04 10:23:32 -0400

    fix(git): fix fixup command

--- nix/modules/hm_modules/dev/git.nix
+++ nix/modules/hm_modules/dev/git.nix
@@ -87,7 +87,7 @@ in
         # easily follow the history of a single file
         file-history = "log -p -M --follow --stat";
         # easily fixup to latest commit
-        fixup = "git commit --fixup=HEAD";
+        fixup = "commit --fixup=HEAD";
         # non-interactively apply the current staged files to a ref
         amend-to = "! f() { git commit --fixup \"$1\" && GIT_SEQUENCE_EDITOR=true git rebase --interactive --autosquash \"$1^\";}; f";
       };
