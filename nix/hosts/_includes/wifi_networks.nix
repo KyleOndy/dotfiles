c544bbf03854c75ee86a82f2b9b1f5a69f07a509
Kyle Ondy <kyle@ondy.org>
2023-09-15 12:26:00 -0400

    feat(wifi): add iphone hotspot

--- nix/hosts/_includes/wifi_networks.nix
+++ nix/hosts/_includes/wifi_networks.nix
@@ -13,6 +13,10 @@
         pskRaw =
           "a2db56e5a0efe7c2e8eaca97d4bc4d872234dda49862554e02ce74696d8306e3";
       };
+      "Kyle's iPhone" = {
+        pskRaw =
+          "1fea8faabb7dc8c28812fa1c9a4251ba0745065490139bcbb0fb7a5a4514a99a";
+      };
       # note: how to connect to unproteced wifi, works with captive portal
       #"GuestNet" = { };
     };
