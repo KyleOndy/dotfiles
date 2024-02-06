bff87d4640cc4f6946488be9ccee88192510b361
Kyle Ondy <kyle@ondy.org>
2024-02-05 23:54:01 -0500

    feat(helios): add script to dotfiles

--- nix/pkgs/default.nix
+++ nix/pkgs/default.nix
@@ -1,5 +1,6 @@
 self: super: {
   concourse = super.callPackage ./concourse { };
+  helios = super.callPackage ./helios { };
   mutt-colors-solarized = super.callPackage ./mutt-colors-solarized { };
   mutt-gruvbox = super.callPackage ./mutt-gruvbox { };
   my-scripts = super.callPackage ./my-scripts { };
