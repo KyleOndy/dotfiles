e752e2d9993e242acf08010fcb292321da7610da
Kyle Ondy <kyle@ondy.org>
2024-02-21 21:41:50 -0500

    feat(foot): add initial module

---
+++ nix/modules/hm_modules/desktop/term/foot.nix
@@ -0,0 +1,32 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.hmFoundry.desktop.term.foot;
+in
+{
+  options.hmFoundry.desktop.term.foot = {
+    enable = mkEnableOption "foot";
+    descrption = "
+    A fast, lightweight and minimalistic Wayland terminal emulator
+
+    https://codeberg.org/dnkl/foot
+    ";
+  };
+
+  config = mkIf cfg.enable {
+    programs.foot = {
+      enable = true;
+      settings = {
+        main = {
+          font = "Hack:size=7";
+          dpi-aware = "yes";
+        };
+        cursor = {
+          blink = "yes";
+        };
+        mouse = {
+          hide-when-typing = "yes";
+        };
+      };
+    };
+  };
+}
