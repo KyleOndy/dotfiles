cdba0959a7892ae63cd9544a5b36f37f8775d256
Kyle Ondy <kyle@ondy.org>
2024-04-01 10:12:17 -0400

    feat(pkgs): add zsh-histdb package derivation

---
+++ nix/pkgs/zsh-histdb/default.nix
@@ -0,0 +1,23 @@
+{ lib, stdenv, fetchgit }:
+
+stdenv.mkDerivation rec {
+  pname = "zsh-histdb";
+  version = "20220118";
+
+  src = fetchGit {
+    url = "https://github.com/larkery/zsh-histdb";
+    rev = "30797f0c50c31c8d8de32386970c5d480e5ab35d";
+  };
+
+  installPhase = ''
+    mkdir -p $out/
+    cp -r . $out/
+  '';
+
+  meta = with lib; {
+    description = "A slightly better history for zsh ";
+    homepage = "https://github.com/larkery/zsh-histdb";
+    platforms = platforms.linux ++ platforms.darwin;
+    license = licenses.mit;
+  };
+}
