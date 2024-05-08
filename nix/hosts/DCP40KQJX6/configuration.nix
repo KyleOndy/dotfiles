f54687ed7473a77dd93fac99aa1dbd48b48a1e04
Kyle Ondy <kyle@ondy.org>
2024-05-08 11:15:41 -0400

    feat: update to nixVersions.latest

--- nix/hosts/DCP40KQJX6/configuration.nix
+++ nix/hosts/DCP40KQJX6/configuration.nix
@@ -15,7 +15,7 @@
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   nix = {
-    package = pkgs.nixUnstable;
+    package = pkgs.nixVersions.latest;
     nixPath = [ "nixpkgs=${pkgs.path}" ]; # todo: this is actaully bad, right?
     extraOptions = ''
       builders-use-substitutes = true
