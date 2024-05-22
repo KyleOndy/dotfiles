9c7195fe4b1eef478ba3ddcfc367bb4b358ace53
Kyle Ondy <kyle@ondy.org>
2024-05-22 11:52:52 -0400

    feat(pi1): add dnsmasq watchdog

--- nix/hosts/pi1/configuration.nix
+++ nix/hosts/pi1/configuration.nix
@@ -40,4 +40,19 @@
       };
     };
   };
+  # TODO: add systemd service to check is we can resolve DNS from self, if not,
+  #       restart dnsmasq. That fact that we hardcode the IP address is
+  #       fragile, but it works for now. I really do need to figure out _why_
+  #       this happens and fix the root issue.
+  systemd.services.dnsmasq-watchdog = {
+    enable = true;
+    startAt = "*-*-* *:*:00"; # every minute
+    path = with pkgs; [
+      dnsutils
+    ];
+    script = ''
+      set -x
+      [[ -z $(dig @10.24.89.53 pi1.lan.509ely.com +short) ]] && systemctl restart dnsmasq || exit 0
+    '';
+  };
 }
