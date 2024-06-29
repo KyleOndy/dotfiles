7c45a23b77e0cd829bf0f59a35617a8b98255dda
Kyle Ondy <kyle@ondy.org>
2024-06-28 21:15:05 -0400

    feat(tf): add reverse proxy in AWS for local services
    Optimum doesn't let me forward some ports, 80 and 443 being not allowed.
    
    I still want to expose some services at simple DNS names, so paying a
    few dollars a month to run a reverse proxy.
    
    I used caddy for the first time, pretty slick.

--- tf/main.tf
+++ tf/main.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 5.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "3.4.3"
     }
   }
 }
