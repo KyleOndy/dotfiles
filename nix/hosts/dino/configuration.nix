e922cbf082f3247d1f0efe4bec14e5648da98054
Kyle Ondy <kyle@ondy.org>
2024-10-03 10:21:49 -0400

    feat(dino): add pipewire for audio

--- nix/hosts/dino/configuration.nix
+++ nix/hosts/dino/configuration.nix
@@ -21,5 +21,12 @@
     enable = true;
   };
 
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 }
 
