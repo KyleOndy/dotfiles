1e32baf3f93eaf1db9bfdb64410533224f12ad80
Kyle Ondy <kyle@ondy.org>
2024-11-17 23:32:01 -0500

    feat(font): add berkeley mono

--- nix/pkgs/default.nix
+++ nix/pkgs/default.nix
@@ -1,4 +1,5 @@
 self: super: {
+  berkeley-mono = super.callPackage ./berkeley-mono { };
   concourse = super.callPackage ./concourse { };
   helios = super.callPackage ./helios { };
   mutt-colors-solarized = super.callPackage ./mutt-colors-solarized { };
