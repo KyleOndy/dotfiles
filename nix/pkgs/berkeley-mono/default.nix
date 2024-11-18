c4ed2e70a310ca630b6dcf7f4db319ced3bdc28d
Kyle Ondy <kyle@ondy.org>
2024-11-17 23:32:01 -0500

    feat(font): add berkeley mono

---
+++ nix/pkgs/berkeley-mono/default.nix
@@ -0,0 +1,20 @@
+# https://berkeleygraphics.com/
+{ lib, stdenv, fetchurl, pkgs }:
+
+pkgs.stdenv.mkDerivation {
+  pname = "berkeley-mono";
+  version = "1.009";
+  src = ./.;
+
+  installPhase = ''
+    # make sure we have decrypted the font
+    for font in *.otf; do
+     file $font | grep --quiet 'OpenType font data'
+    done
+
+    mkdir -p $out/share/fonts/opentype/berkeley-mono
+    cp -r *.otf $out/share/fonts/opentype/berkeley-mono
+
+    runHook postInstall
+  '';
+}
