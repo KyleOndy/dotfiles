5e8e376aa166df285054cbe8f6802f53d54ad2ee
Kyle Ondy <kyle@ondy.org>
2025-01-18 14:13:26 -0500

    feat(tiger): intel arc grpahics

--- nix/hosts/tiger/configuration.nix
+++ nix/hosts/tiger/configuration.nix
@@ -406,18 +406,9 @@ in
       };
     };
 
-  nixpkgs.config.packageOverrides = pkgs: {
-    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
-  };
   hardware = {
     graphics = {
       enable = true;
-      extraPackages = with pkgs; [
-        intel-media-driver # LIBVA_DRIVER_NAME=iHD
-        vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-        vaapiVdpau
-        libvdpau-va-gl
-      ];
     };
   };
 
