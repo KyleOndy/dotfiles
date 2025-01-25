b2fd3c04756b57a9d5c30a99727b4289e2aefc58
Kyle Ondy <kyle@ondy.org>
2025-01-24 22:42:19 -0500

    feat(sources): track master
    Need ceph to build so qemu will build.
    
    https://github.com/NixOS/nixpkgs/issues/369777

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:nixos/nixpkgs/master";
     nixos-hardware.url = "github:NixOS/nixos-hardware/";
     home-manager = {
       url = "github:nix-community/home-manager";
