b45e6976fe7b84dbf9f10e4b8fb3d4491a2a88cf
Kyle Ondy <kyle@ondy.org>
2025-03-26 21:08:23 -0400

    feat(pkgs): set nzbget to master

--- nix/modules/nix_modules/nzbget.nix
+++ nix/modules/nix_modules/nzbget.nix
@@ -57,6 +57,7 @@ in
         # currently all config is done via the web.
         # todo: setup some kind of autoamted downloading of backup zip
         enable = true;
+        package = pkgs.master.nzbget;
         user = cfg.user;
         group = cfg.group;
       };
