e80909012763d1fb18baa2c41b3fdd3cba726dd5
Kyle Ondy <kyle@ondy.org>
2025-06-11 14:45:11 -0400

    feat(zsh): update spaceship-prompt order
    Grabbed it from the latest upstream, added in my worktree sections.
    
    https://github.com/spaceship-prompt/spaceship-prompt/blob/40ec1e7f94cf9987ed424fb58f7d7a66b10e4f38/spaceship.zsh#L42

--- nix/modules/hm_modules/shell/zsh.nix
+++ nix/modules/hm_modules/shell/zsh.nix
@@ -324,27 +324,67 @@ in
           export SPACESHIP_KUBECTL_VERSION_SHOW=false # don't care what version
           export SPACESHIP_WTROOT_PREFIX="for "
           export SPACESHIP_PROMPT_ORDER=(
-            time          # Time stamps section
-            user          # Username section
-            dir           # Current directory section
-            host          # Hostname section
-            wtroot        # hacky homegrown worktree root
-            git           # Git section (git_branch + git_status)
-            golang        # Go section
-            rust          # Rust section
-            haskell       # Haskell Stack section
-            aws           # Amazon Web Services section
-            venv          # virtualenv section
-            python        # python section
-            dotnet        # .NET section
-            kubectl       # Kubectl context section
-            terraform     # Terraform workspace section
-            exec_time     # Execution time
-            line_sep      # Line break
-            battery       # Battery level and status
-            jobs          # Background jobs indicator
-            exit_code     # Exit code section
-            char          # Prompt character
+
+            time           # Time stamps section
+            user           # Username section
+            dir            # Current directory section
+            host           # Hostname section
+            wtroot         # hacky homegrown worktree root
+            git            # Git section (git_branch + git_status + [git_commit](default off))
+            hg             # Mercurial section (hg_branch  + hg_status)
+            package        # Package version
+            node           # Node.js section
+            bun            # Bun section
+            deno           # Deno section
+            ruby           # Ruby section
+            python         # Python section
+            red            # Red section
+            elm            # Elm section
+            elixir         # Elixir section
+            xcode          # Xcode section
+            swift          # Swift section
+            golang         # Go section
+            perl           # Perl section
+            php            # PHP section
+            rust           # Rust section
+            haskell        # Haskell Stack section
+            scala          # Scala section
+            kotlin         # Kotlin section
+            java           # Java section
+            lua            # Lua section
+            dart           # Dart section
+            julia          # Julia section
+            crystal        # Crystal section
+            docker         # Docker section
+            docker_compose # Docker section
+            aws            # Amazon Web Services section
+            gcloud         # Google Cloud Platform section
+            azure          # Azure section
+            venv           # virtualenv section
+            conda          # conda virtualenv section
+            uv             # uv virtualenv section
+            dotnet         # .NET section
+            ocaml          # OCaml section
+            vlang          # V section
+            zig            # Zig section
+            purescript     # PureScript section
+            erlang         # Erlang section
+            gleam          # Gleam section
+            kubectl        # Kubectl context section
+            ansible        # Ansible section
+            terraform      # Terraform workspace section
+            pulumi         # Pulumi stack section
+            ibmcloud       # IBM Cloud section
+            nix_shell      # Nix shell
+            gnu_screen     # GNU Screen section
+            exec_time      # Execution time
+            async          # Async jobs indicator
+            line_sep       # Line break
+            battery        # Battery level and status
+            jobs           # Background jobs indicator
+            exit_code      # Exit code section
+            sudo           # Sudo indicator
+            char           # Prompt character
           )
           export SPACESHIP_GIT_STATUS_SHOW=false
 
