81be921478ed20b74e00d0cc15aaa774828fbda1
Kyle Ondy <kyle@ondy.org>
2025-10-15 23:25:50 -0400

    chore(secrets): add Route53 credentials and streamline SSH authorization
    Add AWS_REGION secret to sops for Route53 DNS management. Consolidate SSH
    authorized keys configuration by deploying only Kyle's personal key to
    remote user accounts.
    
    Changes:
    - Add AWS_REGION to secrets.yaml for Route53 operations
    - Update SSH authorized keys in kyle.nix for remote hosts
    - Remove redundant deployment target key configuration

--- nix/modules/nix_modules/users/kyle.nix
+++ nix/modules/nix_modules/users/kyle.nix
@@ -36,6 +36,7 @@ in
       shell = pkgs.zsh;
       openssh.authorizedKeys.keys = [
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDKPwXdnorhTtQOZ0iE3YJHtb8YYfhjnaav8ArQQuIOQR4tAxPyxMucKHuTsCH3soFFBTY1wg0KVt4x+6op4bfhr0Q40bqQprwy/5LFmui1FZhFhAxrbx4abK0Kh6NaKjvYmV1Lh9+gSKTK9edxWixX90ZI6YHhVEf5JSeUbVcKYKMD4gp5CR5EC2l8/bd/4nQ3n74Od4faa4DfE4qaleEQ4IcAONR0WGxtX1aP2Q4V+UfbS2gvBA0c/V0eIIXnscMcqBbzrYPMxQ7a8umpA65ByHgdFBnCeyvhKjxl2E1HoZcPzruBXs/NqmvnhG6iuFDPtG2G+Lj6xjEYffJcI2VnkYAyczD63P6zlsBIPbyvq7aS8jGR0CsNbfJExjXLmB3M4k2ANBidfai26zAN/Pn73MOA9ieShy1FUZCYf3nM5+EO+0Al6v48eJXNrcUNqKRUHEdyRi+Sd3Nj5shZ61lgCdSZk78XUjXpWcmhbFGaR+9aXn3kUV5rDjqpLzp4alU= kyle@dino"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKtqba65kXXovFMhf0fR02pTlBJ8/w1bj24wqJuQmUZ+ kyle@dino"
       ];
     };
     users.groups.kyle = { };
